39980fba73413456f41706e8cd5ca8f3b2789212
Michael Irwin <mikesir87@gmail.com>
2025-03-12 18:17:40 -0400

    Ensure setup scripts work when directories have spaces in them
    Resolves #50

--- demo/ai-add-service/setup.sh
+++ demo/ai-add-service/setup.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "==> Applying patch and creating a commit"
-git apply --whitespace=fix ${SCRIPT_DIR}/demo.patch
+git apply --whitespace=fix "${SCRIPT_DIR}/demo.patch"
 git commit -am "Modify the compose.yaml to remove kafbat-ui"
